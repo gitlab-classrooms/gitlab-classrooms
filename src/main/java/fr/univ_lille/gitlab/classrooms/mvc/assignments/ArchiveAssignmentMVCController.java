7bfaf6ec7720954d619b1b1b69e4c04252d27e61
Julien WITTOUCK <julien.wittouck@gmail.com>
2025-10-03 18:47:28 +0200

    🚚 : move archive rest controller to mvc package

--- src/main/java/fr/univ_lille/gitlab/classrooms/mvc/assignments/ArchiveAssignmentMVCController.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/mvc/assignments/ArchiveAssignmentMVCController.java
@@ -1,9 +1,10 @@
-package fr.univ_lille.gitlab.classrooms.assignments;
+package fr.univ_lille.gitlab.classrooms.mvc.assignments;
 
+import fr.univ_lille.gitlab.classrooms.assignments.ArchiveAssignmentUseCase;
+import fr.univ_lille.gitlab.classrooms.assignments.AssignmentService;
 import jakarta.annotation.security.RolesAllowed;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,19 +13,19 @@ import java.util.UUID;
 
 @Controller
 @RolesAllowed("TEACHER")
-class ArchiveRestController {
+class ArchiveAssignmentMVCController {
 
     private final AssignmentService assignmentService;
 
     private final ArchiveAssignmentUseCase archiveAssignmentUseCase;
 
-    ArchiveRestController(AssignmentService assignmentService, ArchiveAssignmentUseCase archiveAssignmentUseCase) {
+    ArchiveAssignmentMVCController(AssignmentService assignmentService, ArchiveAssignmentUseCase archiveAssignmentUseCase) {
         this.assignmentService = assignmentService;
         this.archiveAssignmentUseCase = archiveAssignmentUseCase;
     }
 
     @GetMapping("/assignments/{assignmentId}/archive")
-    String archiveAssignment(@PathVariable UUID assignmentId, Model model) {
+    String archiveAssignment(@PathVariable UUID assignmentId) {
         var assignment = this.assignmentService.getAssignment(assignmentId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         this.archiveAssignmentUseCase.archive(assignment);
 
