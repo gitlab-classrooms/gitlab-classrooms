cd61f2e4598cfbaff948cbd07c2b7a8c9a881a84
Julien WITTOUCK <julien@codeka.io>
2026-09-13 11:20:09 +0200

    🐛 : correct refresh-token pre-requisites
    refresh token needed the authorized client to be loaded

--- src/main/java/fr/univ_lille/gitlab/classrooms/gitlab/GitlabApiFactory.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/gitlab/GitlabApiFactory.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.ClientAuthorizationRequiredException;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,11 +18,14 @@ class GitlabApiFactory {
 
     private final OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
 
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(GitlabApiFactory.class);
 
-    public GitlabApiFactory(GitlabProperties gitlabProperties, OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+    public GitlabApiFactory(GitlabProperties gitlabProperties, OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager, OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
         this.gitlabProperties = gitlabProperties;
         this.oAuth2AuthorizedClientManager = oAuth2AuthorizedClientManager;
+        this.oAuth2AuthorizedClientService = oAuth2AuthorizedClientService;
     }
 
     /**
@@ -31,17 +35,22 @@ class GitlabApiFactory {
      * @return
      */
     public GitLabApi userGitlabApi(ClassroomUser user) {
-        var oauth2AuthorizedRequest = OAuth2AuthorizeRequest
-                .withClientRegistrationId("gitlab")
-                .principal(user.getName())
-                .build();
+
         var client = new GitLabApi(gitlabProperties.url(), Constants.TokenType.OAUTH2_ACCESS, "");
         client.setAuthTokenSupplier(() -> {
             try {
+                // load authorized client from database
+                var authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient("gitlab", user.getName());
+                var oauth2AuthorizedRequest = OAuth2AuthorizeRequest
+                        .withAuthorizedClient(authorizedClient)
+                        .principal(user.getName())
+                        .build();
+                // re-authenticate client (this may renew the token using refresh-token)
                 var oauth2Client = oAuth2AuthorizedClientManager.authorize(oauth2AuthorizedRequest);
+
                 LOGGER.debug("Access token for user '{}' is {}", user.getName(), oauth2Client.getAccessToken().getTokenValue());
                 return oauth2Client.getAccessToken().getTokenValue();
-            } catch (ClientAuthorizationRequiredException e) {
+            } catch (IllegalArgumentException | ClientAuthorizationRequiredException e) {
                 LOGGER.error("Unable to get an access token for user '{}'", user.getName());
                 throw new GitLabAuthenticationException("Unable to get an access token for user '%s'".formatted(user.getName()));
             }
